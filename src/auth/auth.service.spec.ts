d799d699edf455f3e88fa33732165c46c32b14dd
saeedjalali <saeedjalalisj@gmail.com>
2020-12-04 14:01:18 +0330

    fix all tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../user/entities/user.schema';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as mongoose from "mongoose";
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  let mongod: MongoMemoryServer = new MongoMemoryServer({
+    autoStart: true,
+  });
+
+  afterEach(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+  });
+
   beforeEach(async () => {
+    mongod = new MongoMemoryServer();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, UserService, LocalStrategy, JwtStrategy],
+      imports:[
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+          secretOrPrivateKey: 'secretKey',
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }),
+        MongooseModule.forRootAsync({
+          useFactory: async () => ({
+            uri: await mongod.getUri(),
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+          }),
+        }),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,14 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as mongoose from "mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.schema';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  let mongod: MongoMemoryServer = new MongoMemoryServer({
+    autoStart: true,
+  });
+
+  afterEach(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+  });
+
   beforeEach(async () => {
+    mongod = new MongoMemoryServer();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
+      imports:[
+        MongooseModule.forRootAsync({
+          useFactory: async () => ({
+            uri: await mongod.getUri(),
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+          }),
+        }),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+      ]
     }).compile();
 
     controller = module.get<UserController>(UserController);
