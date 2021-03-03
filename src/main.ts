919b826fb9fde814929f5e161769bf9b79846bba
saeedjalali <saeedjalalisj@gmail.com>
2021-03-03 17:11:10 +0330

    add helmet + add csurf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TransformInterceptor } from "./shared/transform.interceptor";
 import { ValidationPipe } from "@nestjs/common";
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import { TransformInterceptor } from "./shared/transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
